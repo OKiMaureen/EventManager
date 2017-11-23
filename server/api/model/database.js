33f5d148d56bdb5025f6eb3ec47db1200b965be0
okimaureen <oki.maureen.eloho@gmail.com>
2017-11-23 07:16:57 -0800

    chore(events api): modify server folder

---
+++ server/api/model/database.js
@@ -0,0 +1,17 @@
+const database ={
+"user": [
+    {
+        id:0,
+        eventName:"maureen",
+        eventType:"Golden Center",
+        eventDescription:"hosting the wedding ceremony of mary and john",
+        eventLocation:"Lagos",
+        eventDate:"12/11/34",
+        eventStartTime: "12pm",
+        eventEndTime:"4pm",
+        estimatedAttendees:400,
+        centersAvailable:"1"
+
+    }
+]
+}
