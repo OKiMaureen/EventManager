0de61198a7c95fd0c1e08b214eeaba5519656b45
OkiMaureen <oki.maureen.eloho@gmail.com>
2018-03-30 22:34:07 +0100

    refactor controllers and database

--- server/model/centerDatabase.js
+++ server/model/centerDatabase.js
@@ -1,23 +1,21 @@
 // centers json object
-const centersDb =[
+const centersDb = [
   {
     id: 0,
-		name: "Golden center",
-		address: "3rd Avenue,Festac",
-		capacity: "50-100",
-		cost: 100000,
-		facilities: "air conditioner, projector, standby generator"
-	},
-	{
+    name: 'Golden center',
+    address: '3rd Avenue,Festac',
+    capacity: '50-100',
+    cost: 100000,
+    facilities: 'air conditioner, projector, standby generator'
+  },
+  {
     id: 1,
-		name: "Silver center",
-		address: "Bariga",
-		capacity: "200-300 ",
-		cost: 29000,
-		facilities: "air conditioner, standby generator"
-	}
-]
+    name: 'Silver center',
+    address: 'Bariga',
+    capacity: '200-300 ',
+    cost: 29000,
+    facilities: 'air conditioner, standby generator'
+  }
+];
 export default centersDb;
 
-
-		
--- server/model/eventDatabase.js
+++ server/model/eventDatabase.js
@@ -1,16 +1,16 @@
-//events json object
+// events json object
 const eventsDb = [
-	{
-		id: 0,
+  {
+    id: 0,
     name: "Ama's wedding",
-    type: "social event",
-    location:"Lagos",
-	},
+    type: 'social event',
+    location: 'Lagos',
+  },
   {
     id: 1,
     name: "Joy's wedding",
-    type: "social event",
-		location:"Lagos",
-	}
-]
+    type: 'social event',
+    location: 'Lagos',
+  }
+];
 export default eventsDb;
